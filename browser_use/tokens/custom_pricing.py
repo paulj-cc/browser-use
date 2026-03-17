379e658906a2b40bc71f61b5e8eac9ce3569bfb5
jonathanthe-cc <jonathan@collabocode.com>
2026-03-17 16:20:30 +0700

    Adding custom pricing

--- browser_use/tokens/custom_pricing.py
+++ browser_use/tokens/custom_pricing.py
@@ -27,6 +27,42 @@ CUSTOM_MODEL_PRICING: dict[str, dict[str, Any]] = {
 		'max_input_tokens': None,  # Not specified
 		'max_output_tokens': None,  # Not specified
 	},
+	"openrouter/vertex/deepseek-v3.2": {
+		'input_cost_per_token': 0.56 / 1_000_000,  # $0.60 per 1M tokens
+		'output_cost_per_token': 1.68 / 1_000_000,  # $3.50 per 1M tokens
+		'cache_read_input_token_cost': None, # $0.06 per 1M tokens
+		'cache_creation_input_token_cost': None,  # Not specified
+		'max_tokens': None,  # Not specified
+		'max_input_tokens': None,  # Not specified
+		'max_output_tokens': None,  # Not specified
+	},
+	"openrouter/nvidia/nemotron-3-super-120b-a12b:free": {
+		'input_cost_per_token': 0,  # $0.60 per 1M tokens
+		'output_cost_per_token': 0,  # $3.50 per 1M tokens
+		'cache_read_input_token_cost': 0, # $0.06 per 1M tokens
+		'cache_creation_input_token_cost': 0,  # Not specified
+		'max_tokens': 0,  # Not specified
+		'max_input_tokens': 0,  # Not specified
+		'max_output_tokens': 0,  # Not specified
+	},
+	"openrouter/qwen/qwen3.5-9b": {
+		'input_cost_per_token': 0.10 / 1_000_000,  # $0.60 per 1M tokens
+		'output_cost_per_token': 0.15/ 1_000_000,  # $3.50 per 1M tokens
+		'cache_read_input_token_cost': None, # $0.06 per 1M tokens
+		'cache_creation_input_token_cost': None,  # Not specified
+		'max_tokens': None,  # Not specified
+		'max_input_tokens': None,  # Not specified
+		'max_output_tokens': None,  # Not specified
+	},
+	"vertex/gemini-2.5-flash": {
+		'input_cost_per_token': 0.30 / 1_000_000,  # $0.60 per 1M tokens
+		'output_cost_per_token': 2.50 / 1_000_000,  # $3.50 per 1M tokens
+		'cache_read_input_token_cost': 0.03/ 1_000_000, # $0.06 per 1M tokens
+		'cache_creation_input_token_cost': 0.083/ 1_000_000,  # Not specified
+		'max_tokens': None,  # Not specified
+		'max_input_tokens': None,  # Not specified
+		'max_output_tokens': None,  # Not specified
+	}
 }
 CUSTOM_MODEL_PRICING['bu-latest'] = CUSTOM_MODEL_PRICING['bu-1-0']
 
